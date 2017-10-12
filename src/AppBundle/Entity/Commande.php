a651246e0f72df07df1aed3d06c8a57a9f7d05f4
Apdil <apdil.aydinalp@gmail.com>
2017-10-12 17:23:31 +0200

    generate entity relation

--- src/AppBundle/Entity/Commande.php
+++ src/AppBundle/Entity/Commande.php
@@ -3,6 +3,8 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+
 
 /**
  * Commande
@@ -21,6 +23,23 @@ class Commande
      */
     private $id;
 
+    /**
+    * @ORM\ManyToMany(targetEntity="Preparateur", inversedBy="commandes", cascade={"persist"})
+    * @ORM\JoinTable(name="commande_preparateur")
+    */
+    private $preparateur;
+
+    /**
+    * @ORM\ManyToMany(targetEntity="Client", inversedBy="commandes", cascade={"persist"})
+    * @ORM\JoinTable(name="commande_client")
+    */
+    private $client;
+
+    public function __construct(){
+        $this->preparateur = new ArrayCollection();
+        $this->client = new ArrayCollection();
+    }
+
 
     /**
      * Get id
@@ -31,5 +50,72 @@ class Commande
     {
         return $this->id;
     }
-}
 
+    /**
+     * Add preparateur
+     *
+     * @param \AppBundle\Entity\Preparateur $preparateur
+     *
+     * @return Commande
+     */
+    public function addPreparateur(\AppBundle\Entity\Preparateur $preparateur)
+    {
+        $this->preparateur[] = $preparateur;
+
+        return $this;
+    }
+
+    /**
+     * Remove preparateur
+     *
+     * @param \AppBundle\Entity\Preparateur $preparateur
+     */
+    public function removePreparateur(\AppBundle\Entity\Preparateur $preparateur)
+    {
+        $this->preparateur->removeElement($preparateur);
+    }
+
+    /**
+     * Get preparateur
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getPreparateur()
+    {
+        return $this->preparateur;
+    }
+
+    /**
+     * Add client
+     *
+     * @param \AppBundle\Entity\Client $client
+     *
+     * @return Commande
+     */
+    public function addClient(\AppBundle\Entity\Client $client)
+    {
+        $this->client[] = $client;
+
+        return $this;
+    }
+
+    /**
+     * Remove client
+     *
+     * @param \AppBundle\Entity\Client $client
+     */
+    public function removeClient(\AppBundle\Entity\Client $client)
+    {
+        $this->client->removeElement($client);
+    }
+
+    /**
+     * Get client
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getClient()
+    {
+        return $this->client;
+    }
+}
--- src/AppBundle/Entity/Preparateur.php
+++ src/AppBundle/Entity/Preparateur.php
@@ -3,6 +3,8 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+
 
 /**
  * Preparateur
@@ -34,6 +36,15 @@ class Preparateur
      * @ORM\Column(name="mdp", type="string", length=255)
      */
     private $mdp;
+
+    /**
+    * @ORM\ManyToMany(targetEntity="Commande", mappedBy="preparateur")
+    */
+    private $commandes;
+    
+    public function __construct() {
+        $this->commandes = new ArrayCollection();
+    }
 
 
     /**
@@ -93,5 +104,38 @@ class Preparateur
     {
         return $this->mdp;
     }
-}
 
+    /**
+     * Add commande
+     *
+     * @param \AppBundle\Entity\Commande $commande
+     *
+     * @return Preparateur
+     */
+    public function addCommande(\AppBundle\Entity\Commande $commande)
+    {
+        $this->commandes[] = $commande;
+
+        return $this;
+    }
+
+    /**
+     * Remove commande
+     *
+     * @param \AppBundle\Entity\Commande $commande
+     */
+    public function removeCommande(\AppBundle\Entity\Commande $commande)
+    {
+        $this->commandes->removeElement($commande);
+    }
+
+    /**
+     * Get commandes
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getCommandes()
+    {
+        return $this->commandes;
+    }
+}
